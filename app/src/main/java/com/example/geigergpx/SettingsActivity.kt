7f3defebf8a3e2f1160095487981eadffd7899e7
bocovp <78875347+bocovp@users.noreply.github.com>
2026-03-16 22:38:34 +0400

    Move settings action to toolbar and update settings app bar

--- app/src/main/java/com/example/geigergpx/SettingsActivity.kt
+++ app/src/main/java/com/example/geigergpx/SettingsActivity.kt
@@ -6,10 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Settings"
+
         supportFragmentManager
             .beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
             .commit()
     }
-}
 
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
