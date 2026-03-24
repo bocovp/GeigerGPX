1b35fe01a7cb3c5996ca1e0790d565f2ac0d2646
bocovp <78875347+bocovp@users.noreply.github.com>
2026-03-25 01:28:47 +0400

    Update ConfidenceInterval.kt

--- app/src/main/java/com/example/geigergpx/ConfidenceInterval.kt
+++ app/src/main/java/com/example/geigergpx/ConfidenceInterval.kt
@@ -1,15 +1,70 @@
 package com.example.geigergpx
 
 import java.util.Locale
+import kotlin.div
 import kotlin.math.max
+import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.text.toDouble
+import kotlin.times
 
 class ConfidenceInterval {
-    val mean: Double
-    val delta: Double
-    val lowBound: Double
-    val highBound: Double
+    val mean: Float
+    val delta: Float
+    val lowBound: Float
+    val highBound: Float
     val sampleCount: Int
+
+    // Table[Quantile[ChiSquareDistribution[2 n], alpha/2], {n, 0,11}] /. alpha->1-0.95
+    private val CHI2_L = floatArrayOf(
+        0.0f, 0.0506356f, 0.484419f, 1.23734f, 2.17973f, 3.24697f,
+        4.40379f, 5.62873f, 6.90766f, 8.23075f, 9.59078f, 10.9823f
+    )
+
+    // Table[Quantile[ChiSquareDistribution[2 n], 1-alpha/2], {n, 0,11}] /. alpha->1-0.95
+    private val CHI2_R = floatArrayOf(
+        0.0f, 7.37776f, 11.1433f, 14.4494f, 17.5345f, 20.4832f,
+        23.3367f, 26.1189f, 28.8454f, 31.5264f, 34.1696f, 36.7807f
+    )
+
+    fun chi2(n: Int, z: Float): Float {
+        // val z = normalQuantile(p)
+        // Wilson-Hilferty formula: v * (1 - 2/(9v) + z * sqrt(2/(9v)))^3
+        val term1 = 2.0f / (9.0f * n)
+        val base = 1.0f - term1 + z * sqrt(term1)
+        return n.toFloat() * base.pow(3)
+    }
+
+    fun chi2L(n: Int): Float {
+        return if (n <= 11) CHI2_L[n] else chi2(2 * n, -1.95996f)
+    }
+
+    fun chi2R(n: Int): Float {
+        return if (n <= 11) CHI2_R[n] else chi2(2*n, 1.95996f)
+    }
+
+    constructor(tStart: Double, tEnd: Double, eventsInside: Int, eventAtEnd:Boolean){
+        val duration = (tEnd - tStart).toFloat()
+        require(duration > 0) { "Duration must be positive" }
+        val n = eventsInside         // Total count of events excluding boundaries
+        val add = if (eventAtEnd) 1 else 0
+
+        mean = n / duration
+        val z = 1.95996f // Normal distribution quantile for conf. P = 0.95
+        val root = sqrt((n - add).toFloat())
+        delta = mean * z / root // This is simply CI for normal distribution
+//        val gamma = mean * (z * z - 1.0) / (3 * (n - 1)).toDouble() // This follows from Cornish–Fisher expansion for Chi^2 distribution
+//        lowBound = max(0.0, mean - delta + gamma)
+//        highBound = mean + delta + gamma
+        if (n <= 12) {
+            lowBound = chi2L(n + add) / (2.0f * duration)
+            highBound = chi2R(n + 1) / (2.0f * duration)
+        } else {
+            lowBound = 0.0f // never used
+            highBound = 0.0f // never used
+        }
+    }
+
 
     constructor(mean: Double, delta: Double, lowBound: Double, highBound: Double, sampleCount: Int) {
         this.mean = mean
@@ -18,6 +73,8 @@ class ConfidenceInterval {
         this.highBound = highBound
         this.sampleCount = sampleCount
     }
+
+
 
     constructor(t1: Double, tn: Double, n: Int) {
         // n is number of detected events here so n >= 2
